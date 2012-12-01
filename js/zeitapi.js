ef1322fbf8adaaa68f9df03cac565ebbd817b28c
Matthias Ernst <git@mernst.org>
2012-12-01 17:24:59 +0100

    Api consolidation

--- js/zeitapi.js
+++ js/zeitapi.js
@@ -1,12 +1,36 @@
+function Publication(releaseDate, id) {
+  this.releaseDate = releaseDate;
+  this.id = id;
+}
+
+function AuthorActivity(id, name, publications) {
+  this.id = id;
+  this.name = name;
+  this.publications = publications;
+}
+
 angular.module('zeitapi', ['ng']).config(['$httpProvider', function (http) {
   delete http.defaults.headers.common['X-Requested-With'];
 }]).factory('Author', function ($http) {
+          var prefix = 'http://api.zeit.de/author/';
           var Author = {
             get:function (params, cb) {
-              $http.get('http://api.zeit.de/author/' + params.id + '?fields=uuid,release_date', {
+              $http.get('http://api.zeit.de/author/' + params.id + '?fields=uuid,release_date&limit=1000', {
                 headers:{'X-Authorization':'602c992cc45dd61c013925c253777e31447df5b2ea6e83152283'}
               }).success(function (response) {
-                cb(response);
+                var publications = response.matches.map(function(json) {
+                                          return new Publication(
+                                                  new Date(json.release_date),
+                                                  json.uuid
+                                          );
+                                        });
+                        publications.sort(function(left, right) {
+                          return left.releaseDate.getTime() - right.releaseDate.getTime();
+                        });
+                cb(new AuthorActivity(
+                        response.uri.substring(prefix.length),
+                        response.value,
+                        publications));
               });
             }
           };
